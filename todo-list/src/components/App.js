2f0f2f42cf20995f3b95f4629679f3543a2240c6
mjlee0326 <mjlee032667@gmail.com>
2021-01-04 08:17:05 +0900

    Add button

---
+++ todo-list/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PageTemplate from "./PageTemplate";
+import TodoInput from "./TodoInput";
+
+class App extends Component {
+    render() {
+        return (
+            <PageTemplate>
+                <TodoInput />
+            </PageTemplate>
+        );
+    }
+}
+
+export default App;
