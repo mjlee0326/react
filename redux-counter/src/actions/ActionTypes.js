4e550c1208ef6ede7714dda160434c821ed9e012
mjlee0326 <mjlee032667@gmail.com>
2021-01-24 23:37:45 +0900

    .

--- redux-counter/src/actions/ActionTypes.js
+++ redux-counter/src/actions/ActionTypes.js
@@ -2,5 +2,5 @@ export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const SET_COLOR = "SET_COLOR";
 
-export const CREATE = "create";
-export const REMOVE = "remove";
+export const CREATE = "CREATE";
+export const REMOVE = "REMOVE";
---
+++ redux-counter/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Buttons.css";
+
+const Button = ({ onCreate, onRemove }) => {
+    return <div></div>;
+};
+
+export default Button;
