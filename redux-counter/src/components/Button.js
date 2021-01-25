e1d3e32ed7af6dd101f63bd6c0e31a67c5b9cd40
mjlee0326 <mjlee032667@gmail.com>
2021-01-25 23:50:03 +0900

    button

--- redux-counter/src/components/Button.js
+++ redux-counter/src/components/Button.js
@@ -4,7 +4,25 @@ import PropTypes from "prop-types";
 import "./Buttons.css";
 
 const Button = ({ onCreate, onRemove }) => {
-    return <div></div>;
+    return (
+        <div className="Buttons">
+            <div className="btn add" onClick={onClick}>
+                create
+            </div>
+            <div className="btn remove" onClick={onClick}>
+                remove
+            </div>
+        </div>
+    );
 };
 
+Buttons.PropTypes = {
+    onCreate: PropTypes.func,
+    onRemove: PropTypes.func,
+};
+
+Buttons.defaultProps = {
+    onCreate: () => console.warn("onCreate not defined"),
+    onRemove: () => console.warn("onRemove not defined"),
+};
 export default Button;
